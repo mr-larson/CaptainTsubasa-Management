de64a6359f762f35cce837b4cebe70839f3f62c9
Gauthier <gauthierdewit@gmail.com>
2023-08-19 23:10:10 +0200

    Feature : design and style vue mainMenu

--- resources/js/Pages/MainMenu.vue
+++ resources/js/Pages/MainMenu.vue
@@ -30,26 +30,26 @@ const editDatabase = () => {
             <H2>Main Menu</H2>
         </template>
 
-        <div class="py-12">
+        <div class="mt-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
-                        You're logged in!
+                <div class="flex">
+                    <div class="flex-1 p-2 bg-contain bg-center bg-no-repeat" style="background-image: url('/images/tsubasa.jpg')">
 
+                    </div>
+                    <div class="flex-1 p-2 flex flex-col">
+                        <h1 class="text-5xl font-bold text-gray-800 mb-2">Captain Tsubasa</h1>
+                        <h2 class="text-3xl font-bold text-gray-800 mb-6">Management</h2>
                         <!-- Ajout des boutons pour les nouvelles fonctionnalités -->
-                        <div class="mt-6">
-                            <span class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">New Game</span>
-                            <!-- <button @click="newGame" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        <div class="h-60 mt-6 flex flex-col items-start justify-between">
+                            <button @click="newGame" class="w-60 h-12 bg-sky-400 hover:bg-sky-500 text-2xl text-white font-bold py-2 px-5 border-2 border-sky-600 rounded-full drop-shadow-md mb-2">
                                 New Game
-                            </button> -->
-                            <span class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-4">Load Game</span>
-                            <!-- <button @click="loadGame" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-4">
+                            </button>
+                            <button @click="loadGame" class="w-60 h-12 bg-cyan-400 hover:bg-cyan-500 text-2xl text-white font-bold py-2 px-5 border-2 border-cyan-600 rounded-full drop-shadow-md mb-2">
                                 Load Game
-                            </button> -->
-                            <span class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded ml-4">Edit Database</span>
-                            <!-- <button @click="editDatabase" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-4">
+                            </button>
+                            <button @click="editDatabase" class="w-60 h-12 bg-fuchsia-400 hover:bg-fuchsia-500 text-2xl text-white font-bold py-2 px-5 border-2 border-fuchsia-600 rounded-full drop-shadow-md mb-2">
                                 Edit Database
-                            </button> -->
+                            </button>
                         </div>
                     </div>
                 </div>
