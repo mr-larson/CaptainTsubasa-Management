d05d384d88cdde6247f829bba720f71529fce080
Gauthier <gauthierdewit@gmail.com>
2026-06-30 16:15:27 +0200

    fix(prod): bump Laravel 10.50 + tables sessions/cache pour Laravel Cloud

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -15,13 +16,15 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected static ?string $password = null;
+
     public function definition(): array
     {
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
